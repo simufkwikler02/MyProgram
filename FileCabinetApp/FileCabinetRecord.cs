2fd4d6a6af640dd89e1dedcabb951e085d8ff45e
Oleg Azemsha <bguproga@gmail.com>
2022-02-11 14:17:16 +0300

    Add FileCabinetRecord.cs file.

---
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class FileCabinetRecord
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+}
