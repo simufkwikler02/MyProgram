ff568bc8a001536f048bdcfc5de94761c64e2489
Oleg Azemsha <bguproga@gmail.com>
2022-07-06 18:06:23 +0300

    "Implement CreateRecord for FileCabinetFilesystemService".

--- FileCabinetApp/FileCabinetFilesystemService.cs
+++ FileCabinetApp/FileCabinetFilesystemService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,10 @@ namespace FileCabinetApp
 {
     public class FileCabinetFilesystemService : IFileCabinetService
     {
-        private readonly IRecordValidator validator;
-        private FileStream? fileStream;
         private readonly string serviceRules = "file";
+        private readonly IRecordValidator validator;
+        private readonly int recordSize = 278;
+        private FileStream? fileStream;
 
         public FileCabinetFilesystemService(IRecordValidator validator)
         {
@@ -30,7 +32,54 @@ namespace FileCabinetApp
 
         public int CreateRecord(FileCabinetRecord newRecord)
         {
-            throw new NotImplementedException();
+            var poz = fileStream.Seek(0, SeekOrigin.End);
+            newRecord.Id = (Convert.ToInt32(poz) / this.recordSize) + 1;
+            short status = 0;
+
+            byte[] buffer = Encoding.Default.GetBytes(status.ToString(CultureInfo.CurrentCulture));
+            Array.Resize(ref buffer, 2);
+            this.fileStream.Write(buffer, 0, buffer.Length);
+
+            buffer = Encoding.Default.GetBytes(newRecord.Id.ToString(CultureInfo.CurrentCulture));
+            Array.Resize(ref buffer, 4);
+            this.fileStream.Write(buffer, 0, buffer.Length);
+
+            buffer = Encoding.Default.GetBytes(newRecord.FirstName.ToString(CultureInfo.CurrentCulture));
+            Array.Resize(ref buffer, 120);
+            this.fileStream.Write(buffer, 0, buffer.Length);
+
+            buffer = Encoding.Default.GetBytes(newRecord.LastName.ToString(CultureInfo.CurrentCulture));
+            Array.Resize(ref buffer, 120);
+            this.fileStream.Write(buffer, 0, buffer.Length);
+
+            buffer = Encoding.Default.GetBytes(newRecord.DateOfBirth.Year.ToString(CultureInfo.CurrentCulture));
+            Array.Resize(ref buffer, 4);
+            this.fileStream.Write(buffer, 0, buffer.Length);
+
+            buffer = Encoding.Default.GetBytes(newRecord.DateOfBirth.Month.ToString(CultureInfo.CurrentCulture));
+            Array.Resize(ref buffer, 4);
+            this.fileStream.Write(buffer, 0, buffer.Length);
+
+            buffer = Encoding.Default.GetBytes(newRecord.DateOfBirth.Day.ToString(CultureInfo.CurrentCulture));
+            Array.Resize(ref buffer, 4);
+            this.fileStream.Write(buffer, 0, buffer.Length);
+
+            buffer = Encoding.Default.GetBytes(newRecord.Property1.ToString(CultureInfo.CurrentCulture));
+            Array.Resize(ref buffer, 2);
+            this.fileStream.Write(buffer, 0, buffer.Length);
+
+            buffer = Encoding.Default.GetBytes(newRecord.Property2.ToString(CultureInfo.CurrentCulture));
+            Array.Resize(ref buffer, 16);
+            this.fileStream.Write(buffer, 0, buffer.Length);
+
+            buffer = Encoding.Default.GetBytes(newRecord.Property3.ToString(CultureInfo.CurrentCulture));
+            Array.Resize(ref buffer, 2);
+            this.fileStream.Write(buffer, 0, buffer.Length);
+
+            poz = fileStream.Position;
+            var lenght = fileStream.Length;
+
+            return newRecord.Id;
         }
 
         public void EditRecord(int id, FileCabinetRecord recordEdit)
