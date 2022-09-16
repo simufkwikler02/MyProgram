ddf354670bd0d12a97c90f28f9258d59e4c31f43
Oleg Azemsha <bguproga@gmail.com>
2022-09-16 18:34:09 +0300

    added documentation for Logger and Meter service

--- FileCabinetApp/ServiceLogger.cs
+++ FileCabinetApp/ServiceLogger.cs
@@ -7,10 +7,15 @@ using System.Threading.Tasks;
 
 namespace FileCabinetApp
 {
+    /// <summary>
+    ///    Represents the file cabinet service logger which saves information about service method calls and passed parameters to a text file.
+    /// </summary>
     public class ServiceLogger : IFileCabinetService
     {
-        private IFileCabinetService service;
+        private readonly IFileCabinetService service;
 
+        /// <summary>Initializes a new instance of the <see cref="ServiceLogger" /> class.</summary>
+        /// <param name="service">The file cabinet service.</param>
         public ServiceLogger(IFileCabinetService service)
         {
             this.service = service;
@@ -18,6 +23,12 @@ namespace FileCabinetApp
             fstream.Close();
         }
 
+        /// <summary>Saves information about CreateRecord method to a text file.</summary>
+        /// <param name="newRecord">The new record.</param>
+        /// <returns>
+        ///   New record id.
+        /// </returns>
+        /// <exception cref="System.ArgumentException"> Validation fail - newRecord. </exception>
         public int CreateRecord(FileCabinetRecord newRecord)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -37,6 +48,11 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about DeleteRecord method to a text file.</summary>
+        /// <param name="record">The record that should be deleted.</param>
+        /// <returns>
+        ///   Deleted record id.
+        /// </returns>
         public int DeleteRecord(FileCabinetRecord record)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -56,6 +72,12 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about UpdateRecord method to a text file.</summary>
+        /// <param name="position">Position of the old record.</param>
+        /// <param name="recordUpdate">A new record that will overwrite the old record.</param>
+        /// <returns>
+        ///   Updated record id.
+        /// </returns>
         public int UpdateRecord(long position, FileCabinetRecord recordUpdate)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -75,15 +97,20 @@ namespace FileCabinetApp
             }
         }
 
-        public IEnumerable<FileCabinetRecord> FindByDateoOfBirth(string dateofbirth)
+        /// <summary>Saves information about FindById method to a text file.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>
+        ///   Returns an enumerator.
+        /// </returns>
+        public IEnumerable<FileCabinetRecord> FindById(string id)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
             {
-                fstream.WriteLine($"{DateTime.Now} - Calling FindByDateoOfBirth() with dateofbirth = '{dateofbirth}'");
+                fstream.WriteLine($"{DateTime.Now} - Calling FindById() with id = '{id}'");
                 try
                 {
-                    var result = this.service.FindByDateoOfBirth(dateofbirth);
-                    fstream.WriteLine($"{DateTime.Now} - FindByDateoOfBirth() returned '{result}'");
+                    var result = this.service.FindById(id);
+                    fstream.WriteLine($"{DateTime.Now} - FindById() returned '{result}'");
                     return result;
                 }
                 catch (Exception ex)
@@ -94,6 +121,11 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about FindByFirstName method to a text file.</summary>
+        /// <param name="firstName">The first name.</param>
+        /// <returns>
+        ///   Returns an enumerator.
+        /// </returns>
         public IEnumerable<FileCabinetRecord> FindByFirstName(string firstName)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -113,6 +145,11 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about FindByLastName method to a text file.</summary>
+        /// <param name="lastName">The last name.</param>
+        /// <returns>
+        ///    Returns an enumerator.
+        /// </returns>
         public IEnumerable<FileCabinetRecord> FindByLastName(string lastName)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -132,6 +169,35 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about FindByDateoOfBirth method to a text file.</summary>
+        /// <param name="dateofbirth">The date of birth.</param>
+        /// <returns>
+        ///    Returns an enumerator.
+        /// </returns>
+        public IEnumerable<FileCabinetRecord> FindByDateoOfBirth(string dateofbirth)
+        {
+            using (StreamWriter fstream = new StreamWriter("log.txt", true))
+            {
+                fstream.WriteLine($"{DateTime.Now} - Calling FindByDateoOfBirth() with dateofbirth = '{dateofbirth}'");
+                try
+                {
+                    var result = this.service.FindByDateoOfBirth(dateofbirth);
+                    fstream.WriteLine($"{DateTime.Now} - FindByDateoOfBirth() returned '{result}'");
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    fstream.WriteLine($"{DateTime.Now} - Catch Exception '{ex.Message}'");
+                    throw new ArgumentException($"{ex.Message}");
+                }
+            }
+        }
+
+        /// <summary>Saves information about FindByProperty1 method to a text file.</summary>
+        /// <param name="property1">The property1.</param>
+        /// <returns>
+        ///    Returns an enumerator.
+        /// </returns>
         public IEnumerable<FileCabinetRecord> FindByProperty1(string property1)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -151,6 +217,11 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about FindByProperty2 method to a text file.</summary>
+        /// <param name="property2">The property2.</param>
+        /// <returns>
+        ///   Returns an enumerator.
+        /// </returns>
         public IEnumerable<FileCabinetRecord> FindByProperty2(string property2)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -170,6 +241,11 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about FindByProperty3 method to a text file.</summary>
+        /// <param name="property3">The property3.</param>
+        /// <returns>
+        ///    Returns an enumerator.
+        /// </returns>
         public IEnumerable<FileCabinetRecord> FindByProperty3(string property3)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -189,6 +265,12 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about FindRecords method to a text file.</summary>
+        /// <param name="name">The name of property in the record.</param>
+        /// <param name="value">The value of property in the record, which wiil be found.</param>
+        /// <returns>
+        ///   Returns an enumerator.
+        /// </returns>
         public IEnumerable<FileCabinetRecord> FindRecords(string name, string value)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -208,6 +290,11 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about GetRecord method to a text file.</summary>
+        /// <param name="position">The position.</param>
+        /// <returns>
+        ///  The record by position.
+        /// </returns>
         public FileCabinetRecord? GetRecord(long position)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -227,6 +314,10 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about GetRecords method to a text file.</summary>
+        /// <returns>
+        ///   All records <see langword="ReadOnlyCollection" />.
+        /// </returns>
         public ReadOnlyCollection<FileCabinetRecord> GetRecords()
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -246,6 +337,10 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about GetStat method to a text file.</summary>
+        /// <returns>
+        ///   Count of records.
+        /// </returns>
         public int GetStat()
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -265,6 +360,10 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about GetStatDelete method to a text file.</summary>
+        /// <returns>
+        ///   Count of records deleted.
+        /// </returns>
         public int GetStatDelete()
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -284,6 +383,11 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about IdExist method to a text file.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>
+        ///   <see langword="true"/> if the record with this id exists, <see langword="false"/> otherwise.
+        /// </returns>
         public bool IdExist(int id)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -303,6 +407,10 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about MakeSnapshot method to a text file.</summary>
+        /// <returns>
+        ///   The snapshot of file cabinet service <see cref="FileCabinetServiceSnapshot" />.
+        /// </returns>
         public FileCabinetServiceSnapshot MakeSnapshot()
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -322,6 +430,9 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>
+        /// Saves information about PurgeRecords method to a text file.
+        /// </summary>
         public void PurgeRecords()
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -339,6 +450,8 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about Restore method to a text file.</summary>
+        /// <param name="snapshot">The snapshot of file cabinet service.</param>
         public void Restore(FileCabinetServiceSnapshot snapshot)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -356,6 +469,10 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about ServiceInfo method to a text file.</summary>
+        /// <returns>
+        ///   The name of the validation rules <see cref="string" />.
+        /// </returns>
         public string ServiceInfo()
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -375,6 +492,11 @@ namespace FileCabinetApp
             }
         }
 
+        /// <summary>Saves information about FindIndex method to a text file.</summary>
+        /// <param name="record">The record.</param>
+        /// <returns>
+        ///   The position of the record.
+        /// </returns>
         public long FindIndex(FileCabinetRecord record)
         {
             using (StreamWriter fstream = new StreamWriter("log.txt", true))
@@ -384,25 +506,6 @@ namespace FileCabinetApp
                 {
                     var result = this.service.FindIndex(record);
                     fstream.WriteLine($"{DateTime.Now} - FindIndex() returned '{result}'");
-                    return result;
-                }
-                catch (Exception ex)
-                {
-                    fstream.WriteLine($"{DateTime.Now} - Catch Exception '{ex.Message}'");
-                    throw new ArgumentException($"{ex.Message}");
-                }
-            }
-        }
-
-        public IEnumerable<FileCabinetRecord> FindById(string id)
-        {
-            using (StreamWriter fstream = new StreamWriter("log.txt", true))
-            {
-                fstream.WriteLine($"{DateTime.Now} - Calling FindById() with id = '{id}'");
-                try
-                {
-                    var result = this.service.FindById(id);
-                    fstream.WriteLine($"{DateTime.Now} - FindById() returned '{result}'");
                     return result;
                 }
                 catch (Exception ex)
--- FileCabinetApp/ServiceMeter.cs
+++ FileCabinetApp/ServiceMeter.cs
@@ -9,16 +9,27 @@ using System.Threading.Tasks;
 
 namespace FileCabinetApp
 {
+    /// <summary>
+    ///    Represents the file cabinet service meter which meter the running time of file cabinet service methods.
+    /// </summary>
     public class ServiceMeter : IFileCabinetService
     {
-        private Stopwatch time = new Stopwatch();
-        private IFileCabinetService service;
+        private readonly Stopwatch time = new Stopwatch();
+        private readonly IFileCabinetService service;
 
+        /// <summary>Initializes a new instance of the <see cref="ServiceMeter" /> class.</summary>
+        /// <param name="service">The file cabinet service.</param>
         public ServiceMeter(IFileCabinetService service)
         {
             this.service = service;
         }
 
+        /// <summary>Prints the running time of the CreateRecord method to the console.</summary>
+        /// <param name="newRecord">The new record.</param>
+        /// <returns>
+        ///   New record id.
+        /// </returns>
+        /// <exception cref="System.ArgumentException"> Validation fail - newRecord. </exception>
         public int CreateRecord(FileCabinetRecord newRecord)
         {
             this.time.Reset();
@@ -29,6 +40,11 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the DeleteRecord method to the console.</summary>
+        /// <param name="record">The record that should be deleted.</param>
+        /// <returns>
+        ///   Deleted record id.
+        /// </returns>
         public int DeleteRecord(FileCabinetRecord record)
         {
             this.time.Reset();
@@ -39,6 +55,12 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the UpdateRecord method to the console.</summary>
+        /// <param name="position">Position of the old record.</param>
+        /// <param name="recordUpdate">A new record that will overwrite the old record.</param>
+        /// <returns>
+        ///   Updated record id.
+        /// </returns>
         public int UpdateRecord(long position, FileCabinetRecord recordUpdate)
         {
             this.time.Reset();
@@ -49,16 +71,26 @@ namespace FileCabinetApp
             return result;
         }
 
-        public IEnumerable<FileCabinetRecord> FindByDateoOfBirth(string dateofbirth)
+        /// <summary>Prints the running time of the FindById method to the console.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>
+        ///   Returns an enumerator.
+        /// </returns>
+        public IEnumerable<FileCabinetRecord> FindById(string id)
         {
             this.time.Reset();
             this.time.Start();
-            var result = this.service.FindByDateoOfBirth(dateofbirth);
+            var result = this.service.FindById(id);
             this.time.Stop();
-            Console.WriteLine($"FindByDateoOfBirth method execution duration is {this.time.ElapsedTicks} ticks.");
+            Console.WriteLine($"FindById method execution duration is {this.time.ElapsedTicks} ticks.");
             return result;
         }
 
+        /// <summary>Prints the running time of the FindByFirstName method to the console.</summary>
+        /// <param name="firstName">The first name.</param>
+        /// <returns>
+        ///   Returns an enumerator.
+        /// </returns>
         public IEnumerable<FileCabinetRecord> FindByFirstName(string firstName)
         {
             this.time.Reset();
@@ -69,6 +101,11 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the FindByLastName method to the console.</summary>
+        /// <param name="lastName">The last name.</param>
+        /// <returns>
+        ///    Returns an enumerator.
+        /// </returns>
         public IEnumerable<FileCabinetRecord> FindByLastName(string lastName)
         {
             this.time.Reset();
@@ -79,6 +116,26 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the FindByDateoOfBirth method to the console.</summary>
+        /// <param name="dateofbirth">The date of birth.</param>
+        /// <returns>
+        ///    Returns an enumerator.
+        /// </returns>
+        public IEnumerable<FileCabinetRecord> FindByDateoOfBirth(string dateofbirth)
+        {
+            this.time.Reset();
+            this.time.Start();
+            var result = this.service.FindByDateoOfBirth(dateofbirth);
+            this.time.Stop();
+            Console.WriteLine($"FindByDateoOfBirth method execution duration is {this.time.ElapsedTicks} ticks.");
+            return result;
+        }
+
+        /// <summary>Prints the running time of the FindByProperty1 method to the console.</summary>
+        /// <param name="property1">The property1.</param>
+        /// <returns>
+        ///    Returns an enumerator.
+        /// </returns>
         public IEnumerable<FileCabinetRecord> FindByProperty1(string property1)
         {
             this.time.Reset();
@@ -89,6 +146,11 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the FindByProperty2 method to the console.</summary>
+        /// <param name="property2">The property2.</param>
+        /// <returns>
+        ///   Returns an enumerator.
+        /// </returns>
         public IEnumerable<FileCabinetRecord> FindByProperty2(string property2)
         {
             this.time.Reset();
@@ -99,6 +161,11 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the FindByProperty3 method to the console.</summary>
+        /// <param name="property3">The property3.</param>
+        /// <returns>
+        ///    Returns an enumerator.
+        /// </returns>
         public IEnumerable<FileCabinetRecord> FindByProperty3(string property3)
         {
             this.time.Reset();
@@ -109,6 +176,11 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the FindIndex method to the console.</summary>
+        /// <param name="record">The record.</param>
+        /// <returns>
+        ///   The position of the record.
+        /// </returns>
         public long FindIndex(FileCabinetRecord record)
         {
             this.time.Reset();
@@ -119,6 +191,11 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the GetRecord method to the console.</summary>
+        /// <param name="position">The position.</param>
+        /// <returns>
+        ///  The record by position.
+        /// </returns>
         public FileCabinetRecord? GetRecord(long position)
         {
             this.time.Reset();
@@ -129,6 +206,10 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the GetRecords method to the console.</summary>
+        /// <returns>
+        ///   All records <see langword="ReadOnlyCollection" />.
+        /// </returns>
         public ReadOnlyCollection<FileCabinetRecord> GetRecords()
         {
             this.time.Reset();
@@ -139,6 +220,10 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the GetStat method to the console.</summary>
+        /// <returns>
+        ///   Count of records.
+        /// </returns>
         public int GetStat()
         {
             this.time.Reset();
@@ -149,6 +234,10 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the GetStatDelete method to the console.</summary>
+        /// <returns>
+        ///   Count of records deleted.
+        /// </returns>
         public int GetStatDelete()
         {
             this.time.Reset();
@@ -159,6 +248,11 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the IdExist method to the console.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>
+        ///   <see langword="true"/> if the record with this id exists, <see langword="false"/> otherwise.
+        /// </returns>
         public bool IdExist(int id)
         {
             this.time.Reset();
@@ -169,6 +263,10 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the MakeSnapshot method to the console.</summary>
+        /// <returns>
+        ///   The snapshot of file cabinet service <see cref="FileCabinetServiceSnapshot" />.
+        /// </returns>
         public FileCabinetServiceSnapshot MakeSnapshot()
         {
             this.time.Reset();
@@ -179,6 +277,9 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>
+        /// Prints the running time of the PurgeRecords method to the console.
+        /// </summary>
         public void PurgeRecords()
         {
             this.time.Reset();
@@ -188,6 +289,8 @@ namespace FileCabinetApp
             Console.WriteLine($"PurgeRecords method execution duration is {this.time.ElapsedTicks} ticks.");
         }
 
+        /// <summary>Prints the running time of the Restore method to the console.</summary>
+        /// <param name="snapshot">The snapshot of file cabinet service.</param>
         public void Restore(FileCabinetServiceSnapshot snapshot)
         {
             this.time.Reset();
@@ -197,6 +300,10 @@ namespace FileCabinetApp
             Console.WriteLine($"Restore method execution duration is {this.time.ElapsedTicks} ticks.");
         }
 
+        /// <summary>Prints the running time of the ServiceInfo method to the console.</summary>
+        /// <returns>
+        ///   The name of the validation rules <see cref="string" />.
+        /// </returns>
         public string ServiceInfo()
         {
             this.time.Reset();
@@ -207,6 +314,12 @@ namespace FileCabinetApp
             return result;
         }
 
+        /// <summary>Prints the running time of the FindRecords method to the console.</summary>
+        /// <param name="name">The name of property in the record.</param>
+        /// <param name="value">The value of property in the record, which wiil be found.</param>
+        /// <returns>
+        ///   Returns an enumerator.
+        /// </returns>
         public IEnumerable<FileCabinetRecord> FindRecords(string name, string value)
         {
             this.time.Reset();
@@ -214,16 +327,6 @@ namespace FileCabinetApp
             var result = this.service.FindRecords(name, value);
             this.time.Stop();
             Console.WriteLine($"FindRecords method execution duration is {this.time.ElapsedTicks} ticks.");
-            return result;
-        }
-
-        public IEnumerable<FileCabinetRecord> FindById(string id)
-        {
-            this.time.Reset();
-            this.time.Start();
-            var result = this.service.FindById(id);
-            this.time.Stop();
-            Console.WriteLine($"FindById method execution duration is {this.time.ElapsedTicks} ticks.");
             return result;
         }
     }
